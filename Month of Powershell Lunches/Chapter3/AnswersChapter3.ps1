27c96e868ff32f6113fbcf49907cd54ce8c255a8
supmn084 <47087790+supmn084@users.noreply.github.com>
2019-01-28 20:15:51 -0500

    Chapter 3 answers

---
+++ Month of Powershell Lunches/Chapter3/AnswersChapter3.ps1
@@ -0,0 +1,16 @@
+﻿update-help
+help html
+get-command -Noun file,printer
+get-command -noun -pprocess
+get-command -Verb write -Noun eventlog
+help *alias
+help transcript
+help Get-EventLog -Parameter newest
+help Get-Service -Parameter computername
+help get-process -Parameter computername 
+help out-file -full
+help out-file -full
+get-alias
+ps -c server1
+get-command -noun object
+help *array*
