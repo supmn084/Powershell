2059ad6f1885b7ef8e47c675cb6bc096f0ee5f96
supmn084 <47087790+supmn084@users.noreply.github.com>
2019-01-29 22:35:40 -0500

    Updated Chapter 4 answers

---
+++ Month of Powershell Lunches/Chapter4/Chapter4Lab.ps1
@@ -0,0 +1,7 @@
+﻿Get-Process
+Get-EventLog -LogName Application -Newest 100
+Get-Command -CommandType Cmdlet
+New-Alias -Name Np -Value Notepad.exe
+Get-Service -Name M*
+Show-NetFirewallRule |select name,enabled|OGV
+Show-NetFirewallRule | where {$_.enabled -eq 'true' -AND $_.direction -eq 'inbound'}| select displayname
