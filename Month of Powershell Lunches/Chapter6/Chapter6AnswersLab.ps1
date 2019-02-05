dc92420a324e7e97aa1d1ec5d7852e2f414dcd96
supmn084 <47087790+supmn084@users.noreply.github.com>
2019-02-04 20:49:31 -0500

    added 6th chapter answers powershell stuff

---
+++ Month of Powershell Lunches/Chapter6/Chapter6AnswersLab.ps1
@@ -0,0 +1,19 @@
+﻿#### 1
+"I am the walrus" |out-file file1.txt.
+"I am a beliver" |Out-File file2.txt
+$f1=Get-Content .\file1.txt
+$f2=Get-Content .\file2.txt
+diff $f1 $f2
+####2
+## If you don't specify a filename with out-file you'll get an error, the file is created by Export-Csv ##
+##3
+#yes stop-service and pick the name
+#4
+Get-Service |Export-Csv services.csv -Delimiter "|"
+#5
+-notypeinformation
+#6
+Get-Service |Export-Csv services.csv -NoClobber
+Get-Service |Export-Csv services.csv -Confirm
+#7
+Get-Service |Export-Csv services.csv -UseCulture
