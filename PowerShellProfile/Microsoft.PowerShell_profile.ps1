1c5d7f02faf7c638f79ff8ec0d960247d030052b
supmn084 <47087790+supmn084@users.noreply.github.com>
2019-02-05 22:16:29 -0500

    created initial powershell profile

---
+++ PowerShellProfile/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,18 @@
+﻿## Change to Git Directory for Powershell
+cd C:\dev\Powershell
+# Change the prompt
+function Global:Prompt{
+    $Time = Get-Date -Format "hh:mm:ss"
+    $Directory = (Get-Location).Path.Replace($HOME, "~")
+    
+    Write-Host "[$Time] " -ForegroundColor Yellow -NoNewline
+    Write-Host "$Directory >" -NoNewline
+
+    return " "
+    }
+## Imports and set's up some default troubleshooting utilities for  fun
+Import-Module TroubleshootingPack
+$networkpack=Get-TroubleshootingPack C:\windows\diagnostics\system\Networking
+$speechpack=Get-TroubleshootingPack C:\windows\diagnostics\system\Speech
+$bluescreenpack=Get-TroubleshootingPack C:\windows\diagnostics\system\BlueScreen
+
