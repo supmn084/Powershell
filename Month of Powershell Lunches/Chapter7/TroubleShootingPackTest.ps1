9786415427597a34c7b4ddf4ec15645230d43079
supmn084 <47087790+supmn084@users.noreply.github.com>
2019-02-05 21:49:17 -0500

    added chapter 7 script, diagnostics pack

---
+++ Month of Powershell Lunches/Chapter7/TroubleShootingPackTest.ps1
@@ -0,0 +1,9 @@
+﻿####Get-Module *Trouble* -ListAvailable
+####Import-Module TroubleshootingPack
+
+
+$pack=Get-TroubleshootingPack C:\windows\diagnostics\system\Networking
+Invoke-TroubleshootingPack $pack
+#enter
+#presents a set of options here to test
+#Presents a set of websites to hit or not
