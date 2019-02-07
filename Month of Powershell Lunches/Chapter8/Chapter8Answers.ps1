dfb238038c775959c18806d75be0a64190a3a50e
supmn084 <47087790+supmn084@users.noreply.github.com>
2019-02-06 22:38:23 -0500

    Added new chapter stuff

---
+++ Month of Powershell Lunches/Chapter8/Chapter8Answers.ps1
@@ -0,0 +1,9 @@
+﻿##
+Get-Random
+Get-Date
+system.datetime
+get-date |select dayofweek
+Get-HotFix
+Get-HotFix |sort installedon |select installedon,installedby,hotfixid
+Get-HotFix |sort installedon |select installedon,installedby,hotfixid |ConvertTo-Html -Title "Hotfix Report"|Out-File HotFixReport.htm
+Get-eventlog -LogName System -Newest 50 |sort timegenerated,index|select index,timegenerated,source |outfile elogs.txt
