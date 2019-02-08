c2b7c6a13e02aee01225afc3559604a8b1f85a9e
supmn084 <47087790+supmn084@users.noreply.github.com>
2019-02-07 23:08:03 -0500

    Chapter 9 fun stuff

---
+++ Month of Powershell Lunches/Chapter8/NewADUserCSV.ps1
@@ -0,0 +1,10 @@
+﻿##Get's list of users from CSV and title columns, generates the output to a digestable format for New-ADUser
+Import-Csv -Path 'C:\dev\PowerShell\Month of Powershell Lunches\chapter9\newusers.csv'|
+Select-Object -Property *,
+@{name ='SamaccountName';expression = {$_.login}},
+@{label='Name';expression = {$_.login}},
+@{n ='Department';expression = {$_.Dept}}|New-ADUser
+
+##Run a command on multuple computers
+Get-WmiObject -Class Win32_BIOS -ComputerName ( Get-Content -Path 'C:\dev\PowerShell\Month of Powershell Lunches\chapter9\servers.txt')
+
---
+++ Month of Powershell Lunches/Chapter9/NewADUserCSV.ps1
@@ -0,0 +1,12 @@
+﻿##Get's list of users from CSV and title columns, generates the output to a digestable format for New-ADUser
+Import-Csv -Path 'C:\dev\PowerShell\Month of Powershell Lunches\chapter9\newusers.csv'|
+Select-Object -Property *,
+@{name ='SamaccountName';expression = {$_.login}},
+@{label='Name';expression = {$_.login}},
+@{n ='Department';expression = {$_.Dept}}|New-ADUser
+
+##Run a command on multuple computers
+Get-WmiObject -Class Win32_BIOS -ComputerName ( Get-Content -Path 'C:\dev\PowerShell\Month of Powershell Lunches\chapter9\servers.txt')
+
+#Get Computers in OU
+Get-ADComputer -Filter * -SearchBase "OU=WHERE,OU=STUFF,OU=YOU,OU=WANT,DC=company"
