f5f3cb6af35507d5eb9fb0c4dd781cde334e3752
supmn084 <47087790+supmn084@users.noreply.github.com>
2019-02-07 23:16:01 -0500

    added another thing

---
+++ Month of Powershell Lunches/Chapter9/Get-AdComputerThing.ps1
@@ -0,0 +1,4 @@
+﻿Get-Process -ComputerName (Import-Csv -Path 'C:\dev\Powershell\Month of Powershell Lunches\chapter9\computers.csv'|select -Property hostname)
+
+
+Import-Csv -Path 'C:\dev\Powershell\Month of Powershell Lunches\chapter9\computers.csv'|select -expand hostname |Get-Member
