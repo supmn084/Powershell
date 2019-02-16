065cec0290a3389457a2fe2a6c2d565294dfc61c
supmn084 <47087790+supmn084@users.noreply.github.com>
2019-02-16 16:58:12 -0500

    added array stuff, and started working on stop pending services script

---
+++ StopPendingServicesDump/StopPendingServices.ps1
@@ -0,0 +1,26 @@
+﻿
+    $Services = Get-WmiObject -Class win32_service -Filter "state = 'stop pending'"
+    if ($Services) {
+        foreach ($service in $Services) {
+            try {
+                Stop-Process -Id $service.processid -Force -PassThru -ErrorAction Stop
+            }
+            catch {
+                Write-Warning -Message "Unexpected Error. Error details: $_.Exception.Message"
+            }
+        }
+    }
+    else {
+        Write-Output "There are currently no services with a status of 'Stopping'."
+    }
+}
+
+
+
+
+Invoke-Command -ComputerName PowerShellVM -ScriptBlock ${Function:\Stop-PendingService} -Credential (Get-Credential)
+
+
+
+Enter-PSSession -ComputerName POwershellVM -Credential (Get-Credential)
+
