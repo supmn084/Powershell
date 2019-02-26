29cdd7d9412bbc407ade8aefde40cb76427e0f55
supmn084 <47087790+supmn084@users.noreply.github.com>
2019-02-25 20:42:32 -0500

    removed costart stuff and pushed it into new repo

--- OldCostar Stuff/Multi-Threading/Get-OperatingSystem.ps1
+++
@@ -1,4 +0,0 @@
-# Get-OperatingSystem.ps1 ###
-Param($ComputerName = "LocalHost")
-$x = Get-WmiObject -ComputerName $ComputerName -Class Win32_OperatingSystem 
-write-output "$ComputerName|$($x.SerialNumber)"
