0d633b26ff0f6edbc26db9a50d5f1e192985be59
supmn084 <47087790+supmn084@users.noreply.github.com>
2019-06-15 09:08:32 -0400

    changed to write-output

--- PowerShellExamples/Get-PhysicalAdapters.ps1
+++ PowerShellExamples/Get-PhysicalAdapters.ps1
@@ -4,8 +4,8 @@ Param(
     [Alias('Hostname')]
     [string]$computername
 )
-Write-Verbose "Getting Phyiscal Network Adapters from $computername"
+Write-Output "Getting Phyiscal Network Adapters from $computername"
 Get-WmiObject -Class win32_networkadapter -ComputerName $computername |
 Where-Object { $_physicalAdapter } |
 Select-Object MACAddress, AdapterType, DeviceID, Name, Speed
-Write-Verbose "Finished!"
+Write-Output "Finished!"
