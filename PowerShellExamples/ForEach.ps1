8e3dd1715c418585cb55c921ffeff91469ba26ba
supmn084 <47087790+supmn084@users.noreply.github.com>
2019-06-19 19:40:33 -0400

    ForEach example of cmd

---
+++ PowerShellExamples/ForEach.ps1
@@ -0,0 +1,7 @@
+##Playing with for each cmdlets
+Get-Process | Write-Host $_.name -ForegroundColor Cyan
+#This will fail because it doesn't understand the pipeline
+Get-Process | ForEach-Object { Write-Host $_.Name -ForegroundColor Cyan -BackgroundColor Black }
+#TaDa! now you can to a for each
+
+
