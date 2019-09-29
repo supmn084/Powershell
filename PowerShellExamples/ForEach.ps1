2a8294868da418bc7f1c44e7ec37bfa90acf3bb4
supmn084 <47087790+supmn084@users.noreply.github.com>
2019-09-29 18:02:09 -0400

    Pushing testing change

--- PowerShellExamples/ForEach.ps1
+++ PowerShellExamples/ForEach.ps1
@@ -3,5 +3,6 @@ Get-Process | Write-Host $_.name -ForegroundColor Cyan
 #This will fail because it doesn't understand the pipeline
 Get-Process | ForEach-Object { Write-Host $_.Name -ForegroundColor Cyan -BackgroundColor Black }
 #TaDa! now you can to a for each
+#testing git on new machine
 
 
