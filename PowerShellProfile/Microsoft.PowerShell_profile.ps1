870599ddeab18164757aa52469f905e4fd6330ad
supmn084 <47087790+supmn084@users.noreply.github.com>
2019-10-02 20:34:00 -0400

    Updates for Terraform

--- PowerShellProfile/Microsoft.PowerShell_profile.ps1
+++ PowerShellProfile/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,7 @@
 ﻿## Change to Git Directory for Powershell
-cd C:\dev\Powershell
+cd C:\dev\
 # Change the prompt
-function Global:Prompt{
+function Global:Prompt {
     $Time = Get-Date -Format "hh:mm:ss"
     $Directory = (Get-Location).Path.Replace($HOME, "~")
     
@@ -9,10 +9,7 @@ function Global:Prompt{
     Write-Host "$Directory >" -NoNewline
 
     return " "
-    }
-## Imports and set's up some default troubleshooting utilities for  fun
-Import-Module TroubleshootingPack
-$networkpack=Get-TroubleshootingPack C:\windows\diagnostics\system\Networking
-$speechpack=Get-TroubleshootingPack C:\windows\diagnostics\system\Speech
-$bluescreenpack=Get-TroubleshootingPack C:\windows\diagnostics\system\BlueScreen
+}
+#Alias for terraform binary on machine
+New-Alias terraform "C:\Tools\terraform.exe"
 
